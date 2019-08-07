8dc50066f2a98620ae5465c2038111de070043bc
Maiya Tracy <maiya.tracy@gmail.com>
2019-08-07 13:44:33 -0700

    calc part 2

--- Calculator.java
+++ Calculator.java
@@ -1,49 +1,71 @@
 package com.maiya.calculator;
 
 public class Calculator {
-	private double operandOne;
-	private double operandTwo;
+	private Double operandOne;
+	private Double operandTwo;
 	private String operation;
-	private double result;
+	private Double result;
 
-	public Calculator() {}
-	
-	public Calculator(double operandOne, String operation, double operandTwo) {
+	public Calculator() {
+	}
+
+	public Calculator(Double operandOne, String operation, Double operandTwo) {
 		this.operandOne = operandOne;
 		this.operandTwo = operandTwo;
 		this.operation = operation;
 	}
 
-	public void setOperandOne(double d) {
-		this.operandOne = d;
+	public void performOperation(Double d) {
+		if (this.operandOne == null) {
+			this.operandOne = d;
+		} else {
+			this.operandTwo = d;
+			this.operandOne = this.calculate(this.operandOne, this.operation, this.operandTwo);
+			this.result = this.operandOne;
+			this.operandTwo = null;
+			this.operation = null;
+		}
 	}
 
-	public void setOperation(String string) {
-		this.operation = string;
+	public void performOperation(String entry) {
+		if (entry == "=") {
+			if (this.operandOne != null) {
+				if (this.operandTwo != null && this.operation != null) {
+					this.result = this.calculate(this.operandOne, this.operation, this.operandTwo);
+					
+					this.operandOne = null;
+					this.operandTwo = null;
+					this.operation = null;
+				}
+
+			} else {
+				this.result = this.operandOne;
+			}
+		} else {
+			this.operation = entry;
+		}
 	}
 
-	public void setOperandTwo(double d) {
-		this.operandTwo = d;
-	}
-
-	public void performOperation() {
-		switch (this.operation) {
+	private Double calculate(Double Op1, String Op, Double Op2) {
+		Double result = null;
+		switch (Op) {
 		case "+":
-			this.result = this.operandOne + this.operandTwo;
+			result = Op1 + Op2;
 			break;
 		case "-":
-			this.result = this.operandOne - this.operandTwo;
+			result = Op1 - Op2;
 			break;
 		case "*":
-			this.result = this.operandOne * this.operandTwo;
+			result = Op1 * Op2;
 			break;
 		case "/":
-			this.result = this.operandOne / this.operandTwo;
+			result = Op1 / Op2;
 			break;
 
 		default:
 			break;
 		}
+		return result;
 	}
 
 	public void getResults() {
--- CalculatorTest.java
+++ CalculatorTest.java
@@ -3,16 +3,32 @@ package com.maiya.calculator;
 public class CalculatorTest {
 
 	public static void main(String[] args) {
-		Calculator c = new Calculator();
-		c.setOperandOne(10.5);
-		c.setOperation("+");
-		c.setOperandTwo(5.2);
-		c.performOperation();
-		c.getResults(); //15.7
-		
-		Calculator c1 = new Calculator(10.5,"-",5.2);
-		c1.performOperation();
-		c1.getResults();
+//		Calculator c = new Calculator();
+//		c.setOperandOne(10.5);
+//		c.setOperation("+");
+//		c.setOperandTwo(5.2);
+//		c.performOperation();
+//		c.getResults(); //15.7
+
+		Calculator c2 = new Calculator();
+
+		c2.performOperation(10.5);
+
+		c2.performOperation("+");
+
+		c2.performOperation(5.2);
+
+		c2.performOperation("*");
+
+		c2.performOperation(10.0);
+
+		c2.performOperation("=");
+
+		c2.getResults();
+
+//		Calculator c1 = new Calculator(10.5,"-",5.2);
+//		c1.performOperation();
+//		c1.getResults();
 
 	}
 
